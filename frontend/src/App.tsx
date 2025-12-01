0ecb0b91c3a54e992af042c23fbb224f422f8fc8
Banana-Juice <liao-cheng.jackson@nicholsschool.org>
2025-12-01 00:06:39 -0500

    setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Job Matcher</h1>
+    </div>
+  );
+};
+
+export default App;
